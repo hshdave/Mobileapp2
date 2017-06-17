3fb4fc68cd6c9ccf9bea304bf8844de5c57960bd
Harsh_1694158 <hshdave2011@gmail.com>
2017-06-17 15:31:19 -0400

    Snackbar Done

--- MainActivity.java
+++ MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     public void clicked(View view)
     {
        Snackbar.make(view,"SnackBar",Snackbar.LENGTH_LONG).show();
-
+        //
 
     }
 }
