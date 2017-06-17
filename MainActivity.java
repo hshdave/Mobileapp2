fa0ec708e0e8d0cf812654635de72744dfc23589
Harsh_1694158 <hshdave2011@gmail.com>
2017-06-17 15:26:34 -0400

    Snackbar Done

--- MainActivity.java
+++ MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.a1694158.myapplication;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void clicked(View view)
+    {
+       Snackbar.make(view,"SnackBar",Snackbar.LENGTH_LONG).show();
+
+
     }
 }
